830fc4e70dc057fd5906d2f5b9cd66d1109df572
dadonov <dadonov.igor@gmail.com>
2023-04-14 02:16:05 +0400

    refactor: delete logs and comments

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,11 +1,4 @@
-// const { NotImplementedError } = require("../extensions/index.js");
-
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- */
+const { NotImplementedError } = require("../extensions/index.js");
 
 function transform(array) {
   if (Array.isArray(array)) {
@@ -55,20 +48,7 @@ function transform(array) {
   }
   throw new Error("'arr' parameter must be an instance of the Array!");
 }
-console.log(transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5]));
-console.log(transform([1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5]));
 
-const cases = {
-  doubleDiscarded: {
-    input: [1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5],
-    output: [1, 2, 3, 4, 5],
-  },
-  discardDiscarded: {
-    input: [1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5],
-    output: [1, 2, 3, 4, 5],
-  },
-};
 module.exports = {
   transform,
 };
-
